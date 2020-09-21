ecfab6dad8f3aa1deb9e4dafe3ad5f39790b32d5
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2020-09-21 17:30:32 +0200

    fix env problems

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
 const {resolve} = require('path')
-const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack')
+const Dotenv = require('dotenv-webpack')
 
-
-module.exports = {
-  entry: resolve('./src/Greetings.js'),
-  output: {
-    path: resolve('./dist'),
-    filename: 'bundle.min.js'
-  },
-  watch: true,
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.(jpg|png)$/,
-        use: {
-          loader: "url-loader"
+module.exports = env => {
+  return {
+    entry: resolve('./src/Greetings.js'),
+    output: {
+      path: resolve('./dist'),
+      filename: 'bundle.min.js'
+    },
+    watch: env.WEBPACK_WATCH == "true",
+    mode: env.WEBPACK_MODE,
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
         },
-      }
+        {
+          test: /\.(jpg|png)$/,
+          use: {
+            loader: "url-loader"
+          },
+        }
+      ]
+    },
+    plugins: [
+      new Dotenv()
     ]
-  },
-  plugins: [
-    new Dotenv()
-  ]
+  }
 }
